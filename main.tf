1ef428f435692d4b9c05a0e7383505ea8b214e03
ashinberish <ashinberish@gmaail.com>
2023-04-08 18:25:16 +0530

    2nd instance

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "aws_instance" "demo" {
   ami           = "ami-07d3a50bd29811cd1"
   instance_type = "t2.micro"
 }
+
+resource "aws_instance" "demo1" {
+  ami           = "ami-07d3a50bd29811cd1"
+  instance_type = "t2.micro"
+}
